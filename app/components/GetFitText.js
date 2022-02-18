a57b214ca4ad67a05e25afbb33d1968cdc5bebab
kennytech <kennyemma2008@gmail.com>
2022-02-18 23:41:07 +0100

    Creating home screen

---
+++ app/components/GetFitText.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import styles from '../styles/style';
+
+const getFitText = () => {
+    return (
+        <Text style={styles.getFitText}>Get-Fit</Text>        
+    )
+}
+
+export default getFitText;
---
+++ app/styles/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    getFitText: {
+        fontSize: 48,
+        marginTop: 30,
+        color: '#0037BA',
+        fontWeight: 'bold'
+    },
+});
