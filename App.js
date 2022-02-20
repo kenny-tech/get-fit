9a6e8a32e79d50f5532b2a4cc273441f78faeb40
kennytech <kennyemma2008@gmail.com>
2022-02-20 21:25:29 +0100

    Created profile screen

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ const App = () => {
       <Stack.Navigator 
         initialRouteName="Welcome"
         screenOptions={{
-          headerShown: false
+          headerShown: false,
         }}
       >
         <Stack.Screen
--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
-
+import { View, Image, Text } from 'react-native';
+import Label from '../components/Label';
+import Space from '../components/Space';
 
 const Profile = () => {
 
     return (
         <View style={{flex: 1, alignItems:'flex-start', backgroundColor:'#0037BA'}}>
-            <Text>This is the Profile screen</Text>
+            <View style={{flexDirection: 'row', marginLeft: 40, marginTop: 20}}>
+                <Image
+                    source={require('../images/profile.png')}
+                />    
+                <Text style={{paddingLeft: 10, paddingTop: 50, fontWeight: 'bold', fontSize: 22}}>Michael Mololuwa</Text>
+            </View>
+            <View style={{alignItems: 'center', width: '100%'}}>
+                <Space />
+                <Label labelTextColor="#FFFFFF" labelText="Account settings" labelSize={22} />
+                <Space />
+                <Label labelTextColor="#FFFFFF" labelText="Cart" labelSize={22} />
+                <Space />
+                <Label labelTextColor="#FFFFFF" labelText="Track your order" labelSize={22} />
+                <Space />
+                <Label labelTextColor="#FFFFFF" labelText="Language settings" labelSize={22} />
+                <Space />
+                <Label labelTextColor="#FFFFFF" labelText="Change password" labelSize={22} />
+                <Space />
+                <Label labelTextColor="#FFFFFF" labelText="Log out" labelSize={22} />
+                <Space />
+            </View>
         </View>
     )
 }
