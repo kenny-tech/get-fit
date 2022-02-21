53d330c0fbfbe52fe49b456818c06a282a43b9d3
kennytech <kennyemma2008@gmail.com>
2022-02-21 09:37:42 +0100

    Wishlist screen in progress

---
+++ app/components/Line.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Line = () => {
+    return (
+        <View
+            style={{
+                borderBottomColor: '#000',
+                borderBottomWidth: 1,
+                marginLeft: 50,
+                marginTop: 10,
+                width: 250
+            }}
+        />
+    )
+}
+
+export default Line;
